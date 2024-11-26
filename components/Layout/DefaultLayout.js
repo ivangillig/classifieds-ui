247ed80d23b32984ea67f6b710cd135aa5fee8f7
Ivan Gillig <ivan.gillig@gmail.com>
2024-11-26 14:19:23 -0300

    styles updated - antd layout added

--- components/Layout/DefaultLayout.js
+++ components/Layout/DefaultLayout.js
@@ -1,21 +1,42 @@
 // components/DefaultLayout.js
-import React from 'react';
-import Head from './Head';
-import AppFooter from './Footer';
-import NavBar from './NavBar';
+import React from "react";
+import { Layout } from "antd";
+import Head from "./Head";
+import AppFooter from "./Footer";
+import NavBar from "./NavBar";
+
+const { Header, Content, Footer } = Layout;
 
 const DefaultLayout = ({ children, title }) => {
   return (
     <div className="layout">
       <Head title={title} />
-      
-      <NavBar />
 
-      <main className="main-container">
-        {children}
-      </main>
+      {/* Header/NavBar */}
+      <Header
+        className="navbar-header"
+        style={{
+          position: "sticky",
+          top: 0,
+          zIndex: 10,
+          width: "100%",
+          height: "20px",
+          lineHeight: "30px",
+          padding: "0",
+        }}
+      >
+        <NavBar />
+      </Header>
 
-      <AppFooter />
+      {/* Main Content */}
+      <Content style={{ flex: "1 0 auto", padding: "24px", marginTop: "50px" }}>
+        <main className="main-container">{children}</main>
+      </Content>
+
+      {/* Footer */}
+      <Footer style={{ flexShrink: 0, backgroundColor: "none" }}>
+        <AppFooter />
+      </Footer>
     </div>
   );
 };
