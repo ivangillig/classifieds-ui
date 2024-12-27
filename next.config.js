ba6bbd295a78ec3a3dcf1f31ae1c4570300a2d1d
Ivan Gillig <ivan.gillig@gmail.com>
2024-12-27 13:04:44 -0300

    adjust on i18n config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
-const withLess = require("next-with-less");
-const { i18n } = require('./next-i18next.config')
+const withLess = require('next-with-less')
 
 module.exports = withLess({
-  i18n,
-  reactStrictMode: true,
+  i18n: {
+    locales: ['es', 'en'],
+    defaultLocale: 'es',
+    localeDetection: false,
+  },
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
   publicRuntimeConfig: {
-    BASE_URL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5000",
-    listingImagesBasePath: process.env.IMG_BASE_PATH || "/uploads/",
+    BASE_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000',
+    listingImagesBasePath: process.env.IMG_BASE_PATH || '/uploads/',
   },
   compiler: {
     styledComponents: true,
@@ -16,12 +19,23 @@ module.exports = withLess({
       javascriptEnabled: true,
       // TODO: This is not working
       modifyVars: {
-        '@primary-color': '#5d40dd', 
+        '@primary-color': '#5d40dd',
         '@link-color': '#7b63e6',
         '@font-size-base': '14px',
         '@border-radius-base': '4px',
       },
     },
   },
-  transpilePackages: [ "antd", "@ant-design", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table", "rc-input" ]
-});
+  transpilePackages: [
+    'antd',
+    '@ant-design',
+    'rc-util',
+    'rc-pagination',
+    'rc-picker',
+    'rc-notification',
+    'rc-tooltip',
+    'rc-tree',
+    'rc-table',
+    'rc-input',
+  ],
+})
